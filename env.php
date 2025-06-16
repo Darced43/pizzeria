688f1697ea908a2b8d213b08488ce192c044371d
Darced43 <94393410+Darced43@users.noreply.github.com>
2025-06-16 13:31:54 +0300

    Update env.php

--- env.php
+++ env.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'DB_HOST' => 'localhost',
-    'DB_USER' => 'root',
-    'DB_NAME' => 'pizzeria',
-    'DB_PASSWORD' => '123456',
+    'DB_USER' => 'userName',
+    'DB_NAME' => 'dbName',
+    'DB_PASSWORD' => 'password',
 ];
